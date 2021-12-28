575aff36ffa31ba55983c72b217bc5c32364cdeb
yuvalmo <yuvalmoalem24@gmail.com>
2021-12-28 16:04:23 +0200

    [image] create initial PPM image.
    Also added top cmake file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rt
 Simple ray tracer. Credit to "Ray Tracing in One Weekend". 
+
+# Guidelines
+* Type the code myself.
+* Do not copy-paste code without understanding it first.
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+    // Dimensions
+    const int width = 256;
+    const int height = 256;
+
+    // Create PPM image
+    
+    // Header
+    std::cout << "P3\n"
+              << width << ' ' << height << '\n'
+              << "255\n";
+
+    for (int y = height - 1; y >= 0; --y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            // Factor between 0 and 1
+            const auto r = static_cast<double>(x) / (width - 1);
+            const auto g = static_cast<double>(y) / (height - 1);
+            const auto b = 0.25;
+
+            // Colors
+            const int ir = static_cast<int>(255.999 * r);
+            const int ig = static_cast<int>(255.999 * g);
+            const int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
